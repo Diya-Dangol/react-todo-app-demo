1738888a7567f0fdb27fba4ca3bb8374af6928bc
Diya-Dangol <diya.dangol15@gmail.com>
2020-08-28 16:35:52 +0545

    Updates

---
+++ precache-manifest.77fa614c51b78d3deb9afc88bfb0191a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139888804168456f53d499188704e880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa58caefbc75ca32a03",
+    "url": "/static/css/main.11e68394.chunk.css"
+  },
+  {
+    "revision": "164e30f936b5cbe8d616",
+    "url": "/static/js/2.880ee714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.880ee714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa58caefbc75ca32a03",
+    "url": "/static/js/main.33353354.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
